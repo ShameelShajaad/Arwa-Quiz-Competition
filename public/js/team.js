9f2b3d977ecf352e2445e4d7b90bf2db488f631d
ShameelShajaad <shameels1228@gmail.com>
2026-08-04 23:41:20 +0530

    rewrited team.js code

--- public/js/team.js
+++ public/js/team.js
@@ -1,7 +1,19 @@
 const socket = io();
 
-socket.on("timer", (time) => {
+socket.on("timer",(time)=>{
 
-    document.getElementById("timer").innerHTML = time;
+document.getElementById("timer").innerHTML=time;
+
+});
+
+socket.on("question",(q)=>{
+
+document.getElementById("question").innerHTML=q.question;
+
+for(let i=0;i<4;i++){
+
+document.getElementById("a"+i).innerHTML=q.options[i];
+
+}
 
 });
